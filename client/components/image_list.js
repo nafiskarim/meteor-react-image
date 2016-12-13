af995c2b51bf7b203c8b8df970b9feff8291bffd
nafiskarim <sahil.abrar@gmail.com>
2016-12-13 10:30:16 +0100

    first react component - ImageList

---
+++ client/components/image_list.js
@@ -0,0 +1,15 @@
+// our image list component
+import React from 'react'
+
+var ImageList = () => {
+
+  return (
+      <div>
+        <ul>
+          <li>Image detaisl</li>
+        </ul>
+      </div>
+    )
+  }
+
+export default ImageList
