eed2353903ff3dc87ebb61efa0b6f96b42c06595
nafiskarim <sahil.abrar@gmail.com>
2016-12-13 12:41:40 +0100

    app rerender when ajax request is completed

--- client/main.js
+++ client/main.js
@@ -4,12 +4,17 @@ import axios from 'axios'
 import ImageList from './components/image_list.js'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { images: [] }
+  }
   componentWillMount() {
     // great place to load our data
     axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
-    .then( (response) => console.log(response))
+    .then( (response) => this.setState({ images: response.data.data}))
   }
   render() {
+    console.log(this.state.images);
     return (
       <div>
         <ImageList />
