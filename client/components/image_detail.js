f90de36fd4868664633b31a2921b7753b97ad7d8
nafiskarim <sahil.abrar@gmail.com>
2016-12-13 13:48:41 +0100

    image score component and style added

--- client/components/image_detail.js
+++ client/components/image_detail.js
@@ -1,14 +1,19 @@
 import React, { PropTypes } from 'react'
+import ImageScore from './image_score'
 
 const ImageDetail = (props) => {
     return (
       <li className="media list-group-item">
         <div className="media-body">
           <h1 className="media-heading">{props.image.title}</h1>
+          <h4>{props.image.description}</h4>
+          <h4>{props.image.ups}</h4>
+          <h4>{props.image.downs}</h4>
         </div>
         <div className="media-left">
           <img src={props.image.link}/>
         </div>
+        <ImageScore ups={props.image.ups} downs={props.image.downs} />
       </li>
     )
 }
